ad6c2253034d063d6b1973f27fb0b13e73f1e4ff
viensolas <102194349+viensolas@users.noreply.github.com>
2023-06-23 14:36:55 -0500

    navbar css done

---
+++ src/app/new/page.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import page from "../search/page";
+
+export default function page() {
+  return <div>this is new page</div>;
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,51 @@
+"use client";
+import Link from "next/link";
+import SearchFillIcon from "./ui/icons/SearchFillIcon";
+import SearchIcon from "./ui/icons/SearchIcon";
+import HomeIcon from "./ui/icons/HomeIcon";
+import HomeFillIcon from "./ui/icons/HomeFillIcon";
+import NewIcon from "./ui/icons/NewIcon";
+import NewFillIcon from "./ui/icons/NewFillIcon";
+import { usePathname } from "next/navigation";
+import ColorButton from "./ui/ColorButton";
+
+const menu = [
+  {
+    href: "/",
+    icon: <HomeIcon />,
+    clickedIcon: <HomeFillIcon />,
+  },
+  {
+    href: "/search",
+    icon: <SearchIcon />,
+    clickedIcon: <SearchFillIcon />,
+  },
+  {
+    href: "/new",
+    icon: <NewIcon />,
+    clickedIcon: <NewFillIcon />,
+  },
+];
+
+export default function navbar() {
+  const pathName = usePathname();
+  return (
+    <div className="flex justify-between items-center px-6">
+      <Link href="/">
+        <h1 className="text-3xl font-bold">Instantgram</h1>
+      </Link>
+      <nav>
+        <ul className="flex gap-4 items-center p-4">
+          {menu.map((item) => (
+            <li key={item.href}>
+              <Link href={item.href}>
+                {pathName === item.href ? item.clickedIcon : item.icon}
+              </Link>
+            </li>
+          ))}
+          <ColorButton text="Sign in" onClick={() => {}} />
+        </ul>
+      </nav>
+    </div>
+  );
+}
