a169667e2bb86fe5557239a1f6791851f994c4cc
viensolas <102194349+viensolas@users.noreply.github.com>
2023-07-15 08:36:08 -0500

    following bar done

--- src/components/ui/Avartar.tsx
+++ src/components/ui/Avartar.tsx
@@ -15,7 +15,7 @@ export default function Avartar({
     <div className={getContainerStyle(size, highlight)}>
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
-        className={`bg-white rounded-full p-[0.1rem] ${getImageSizeStyle(
+        className={`bg-white object-cover rounded-full p-[0.1rem] ${getImageSizeStyle(
           size
         )}`}
         alt="user profile"
@@ -38,5 +38,5 @@ function getContainerStyle(size: string, highlight: boolean): string {
 function getImageSizeStyle(size: string): string {
   return size === "small"
     ? "w-[34px] h-[34px] p-[0.1rem]"
-    : "w-16 h-16 p-[0.2rem";
+    : "w-16 h-16 p-[0.2rem]";
 }
--- src/components/ui/FollowingBar.tsx
+++ src/components/ui/FollowingBar.tsx
@@ -1,5 +1,40 @@
+"use client";
+import { DetailUser } from "@/model/user";
 import React from "react";
+import { PropagateLoader } from "react-spinners";
+import useSWR from "swr";
+import page from "../../app/search/page";
+import Link from "next/link";
+import Avartar from "./Avartar";
+import ScrollableBar from "./ScrollableBar";
 
 export default function FollowingBar() {
-  return <div>following bar</div>;
+  const { data, isLoading: loading, error } = useSWR<DetailUser>("api/me");
+  const users = data?.following;
+
+  return (
+    <section className="w-full flex justify-center items-center p-4 shadow-sm shadow-neutral-300 mb-4 rounded-lg min-h-[90px] overflow-x-auto">
+      {loading ? (
+        <PropagateLoader size={8} color="red" />
+      ) : (
+        (!users || users.length === 0) && <p>'You don't have following'</p>
+      )}
+      {users && users.length > 0 && (
+        <ScrollableBar>
+          {users.map(({ image, username }) => (
+            <Link
+              key={username}
+              className="flex flex-col items-center w-20"
+              href={`/user/${username}`}
+            >
+              <Avartar image={image} highlight />
+              <p className="w-full text-sm text-center text-ellipsis overflow-hidden">
+                {username}
+              </p>
+            </Link>
+          ))}
+        </ScrollableBar>
+      )}
+    </section>
+  );
 }
