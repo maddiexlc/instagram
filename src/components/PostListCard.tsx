5af91bfaecabb2631c64e7df6ad390dd55dc8aea
viensolas <102194349+viensolas@users.noreply.github.com>
2023-07-23 13:03:49 -0500

    post detail done. before user search

--- src/components/PostListCard.tsx
+++ src/components/PostListCard.tsx
@@ -1,14 +1,15 @@
-'use client';
+"use client";
 
-import { SimplePost } from '@/model/post';
-import Image from 'next/image';
-import { useState } from 'react';
-import ActionBar from './ActionBar';
-import Avatar from './Avatar';
-import CommentForm from './CommentForm';
-import PostDetail from './PostDetail';
-import PostModal from './PostModal';
-import ModalPortal from './ui/ModalPortal';
+import { SimplePost } from "@/model/post";
+import Image from "next/image";
+import { useState } from "react";
+import ActionBar from "./ActionBar";
+import Avatar from "./Avatar";
+import CommentForm from "./CommentForm";
+import PostDetail from "./PostDetail";
+import PostModal from "./PostModal";
+import ModalPortal from "./ui/ModalPortal";
+import PostUserAvatar from "./PostUserAvatar";
 
 type Props = {
   post: SimplePost;
@@ -20,13 +21,10 @@ export default function PostListCard({ post, priority = false }: Props) {
   const [openModal, setOpenModal] = useState(false);
 
   return (
-    <article className='rounded-lg shadow-md border border-gray-200'>
-      <div className='flex items-center p-2'>
-        <Avatar image={userImage} size='medium' highlight />
-        <span className='text-gray-900 font-bold ml-2'>{username}</span>
-      </div>
+    <article className="rounded-lg shadow-md border border-gray-200">
+      <PostUserAvatar image={userImage} username={username} />
       <Image
-        className='w-full object-cover aspect-square'
+        className="w-full object-cover aspect-square"
         src={image}
         alt={`photo by ${username}`}
         width={500}
--- src/components/PostModal.tsx
+++ src/components/PostModal.tsx
@@ -1,4 +1,4 @@
-import CloseIcon from './ui/icons/CloseIncon';
+import CloseIcon from "./ui/icons/CloseIncon";
 
 type Props = {
   children: React.ReactNode;
@@ -7,7 +7,7 @@ type Props = {
 export default function PostModal({ onClose, children }: Props) {
   return (
     <section
-      className='fixed top-0 left-0 flex flex-col justify-center items-center w-full h-full z-50 bg-neutral-900/70'
+      className="fixed top-0 left-0 flex flex-col justify-center items-center w-full h-full z-50 bg-neutral-900/70"
       onClick={(event) => {
         if (event.target === event.currentTarget) {
           onClose();
@@ -15,12 +15,12 @@ export default function PostModal({ onClose, children }: Props) {
       }}
     >
       <button
-        className='fixed top-0 right-0 p-8 text-white'
+        className="fixed top-0 right-0 p-8 text-white"
         onClick={() => onClose()}
       >
         <CloseIcon />
       </button>
-      {children}
+      <div className="bg-white w-4/5 h-3/5 max-w-7xl"> {children}</div>
     </section>
   );
 }
